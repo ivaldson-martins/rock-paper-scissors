987255ecd1a050959db2cadcfd1922fe3bef7eb9
Ivaldson Pinheiro <ivaldson.martins@gmail.com>
2021-08-24 12:08:08 -0300

    UI adicionada e edição do README.md

---
+++ readme.md
@@ -0,0 +1,4 @@
+exercicio para fazer um jogo de pedra papel e tesoura
+ainda não sei escrever README.md e estou upando modificações no git que não são atomicas
+ou seja muitas alterações de uma vez só, dificultando assim a identificação de bugs por
+commits realizados.
--- script-game.js
+++ script-game.js
@@ -1,56 +1,78 @@
+let counterComputer = 0;
+let counterPlayer = 0;
+const message = document.querySelector('.message');
+const divResults = document.querySelector('.score');
+
+message.textContent = "Clique em qualquer botao para começar";
+divResults.textContent = '\nHuman ' + counterPlayer + ' : ' + counterComputer + ' Machine';
+
 function computerPlay() {
-    let arrayDeJogadas = ['rock', 'paper', 'scissors'];
-    let jogada = arrayDeJogadas[Math.floor(Math.random()*arrayDeJogadas.length)];
-    return jogada;
- }
- function playRound(playerSelection, computerSelection) {
-    let playerSelectionConverted = playerSelection.toLowerCase();
-    if (playerSelectionConverted === computerSelection) {
-       return "It's a tie!";
-    }
-    if (playerSelectionConverted == 'rock' && computerSelection == 'scissors') {
-       return 'You win! Rock beats scissors';
-    } else if (playerSelectionConverted == 'rock' && computerSelection == 'paper') {
-       return 'You lose! Paper beats Rock';
+   let arrayDeJogadas = ['rock', 'paper', 'scissors'];
+   let jogada = arrayDeJogadas[Math.floor(Math.random() * arrayDeJogadas.length)];
+   return jogada;
+}
 
-    } else if (playerSelectionConverted == 'paper' && computerSelection == 'scissors') {
-       return 'You lose! scissors beats paper';
+function playRound(e) {
+   let computerSelection = computerPlay();
+   let playerSelectionConverted = e.srcElement.id;
+   // imprime as jogadas do computador e as minhas no console
+   console.log(computerSelection, playerSelectionConverted);
+   if (playerSelectionConverted === computerSelection) {
+      score("It's a tie!");
+   }
+   if (playerSelectionConverted == 'rock' && computerSelection == 'scissors') {
+      score('You win! Rock beats scissors');
+   } else if (playerSelectionConverted == 'rock' && computerSelection == 'paper') {
+      score('You lose! Paper beats Rock');
 
-    } else if (playerSelectionConverted == 'paper' && computerSelection == 'rock') {
-       return 'You win! Paper beats Rock';
+   } else if (playerSelectionConverted == 'paper' && computerSelection == 'scissors') {
+      score('You lose! scissors beats paper');
 
-    } else if (playerSelectionConverted == 'scissors' && computerSelection == 'paper') {
-       return 'You win! scissors beats paper';
-    } else if (playerSelectionConverted == 'scissors' && computerSelection == 'rock') {
-       return 'You lose! Rock beats scissors';
-    }
- }
- function game() {
-    let counterPlayer = 0;
-    let counterComputer = 0;
-    let resultRound = '';
-    let playHuman = '';
-    for (let i = 0; i < 5; i++) {
-       playHuman = window.prompt('Rock, paper or scissors?')
-       resultRound = playRound(playHuman, computerPlay());
-       if (resultRound.search('win') != -1) {
-          ++counterPlayer;
-          console.log('The humam have won the round');
-       } else if (resultRound.search('lose') != -1){
-          ++counterComputer;
-          console.log('The machines have won the round');
-       }else {
-          console.log("It's a tie");  
-       }
-    }
-    if (counterPlayer > counterComputer) {
-       console.log('The humam have won the match');
-    } else if (counterPlayer < counterComputer) {
-       console.log('The machines have won the match');
-    } else {
-       console.log("It's a tie");
-    }
- }
+   } else if (playerSelectionConverted == 'paper' && computerSelection == 'rock') {
+      score('You win! Paper beats Rock');
 
- game();
- 
+   } else if (playerSelectionConverted == 'scissors' && computerSelection == 'paper') {
+      score('You win! scissors beats paper');
+   } else if (playerSelectionConverted == 'scissors' && computerSelection == 'rock') {
+      score('You lose! Rock beats scissors');
+   }
+}
+
+function score(resultRound) {
+   let text = '';
+   if (resultRound.search('win') != -1) {
+      ++counterPlayer;
+      text = 'The humam have won the round';
+   } else if (resultRound.search('lose') != -1) {
+      ++counterComputer;
+      text = 'The machines have won the round';
+   } else {
+      text = "It's a tie";
+   }
+   checkVictory();
+   message.textContent = text;
+   divResults.textContent = '\nHuman ' + counterPlayer + ' : ' + counterComputer + ' Machine';
+}
+
+function checkVictory() {
+   if (counterComputer===5 || counterPlayer===5){
+      message.textContent = "Clique em qualquer botao para começar";
+      if (counterPlayer > counterComputer) {
+         alert('The humam have won the match');
+      } else if (counterPlayer < counterComputer) {
+         alert('The machines have won the match');
+      }
+      
+      counterPlayer = 0;
+      counterComputer = 0;
+   }
+}
+
+function game() {
+   const buttons = document.querySelectorAll('button');
+   buttons.forEach((button) => {
+      button.addEventListener('click', playRound);
+   });
+}
+
+game();
